39102a31196192fba49fff602a55bbba782c45cf
ssxfiij <sofia.araya663@gmail.com>
2025-08-22 14:10:04 -0400

    Subir ejemplo de for

--- clase.kt
+++ clase.kt
@@ -1,12 +1,11 @@
 fun main(){
     for (i in 1..20) {
-        if (i == 5) {
+        if (i == 5 || (i > 10 && i < 20)) {
         }
         else {
                 println(i)
             }
         }
-        println(i)
     }
 
 
